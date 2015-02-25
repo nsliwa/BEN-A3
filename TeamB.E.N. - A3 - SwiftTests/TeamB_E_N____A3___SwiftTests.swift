72d549927a479f35f1b30d3ae3705eef22c68678
nsliwa <nsliwa@smu.edu>
2015-02-25 15:53:01 -0600

    NVM - forgot we’re doing it in swift

---
+++ TeamB.E.N. - A3 - SwiftTests/TeamB_E_N____A3___SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  TeamB_E_N____A3___SwiftTests.swift
+//  TeamB.E.N. - A3 - SwiftTests
+//
+//  Created by Nicole Sliwa on 2/25/15.
+//  Copyright (c) 2015 Team B.E.N. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TeamB_E_N____A3___SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
