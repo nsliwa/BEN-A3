b3db201d6cea8a054d9fe847069d6027874c6921
nsliwa <nsliwa@smu.edu>
2015-02-25 16:24:29 -0600

    Created base storyboard

---
+++ TeamB.E.N. - A3 - Swift/MasterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MasterViewController.swift
+//  TeamB.E.N. - A3 - Swift
+//
+//  Created by Nicole Sliwa on 2/25/15.
+//  Copyright (c) 2015 Team B.E.N. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
