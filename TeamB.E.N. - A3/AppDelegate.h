eaf617f287d827083aa1194e6e0d07a44fa1bfb6
nsliwa <nsliwa@smu.edu>
2015-02-25 15:46:58 -0600

    Initial Commit
    Base Project Created

---
+++ TeamB.E.N. - A3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TeamB.E.N. - A3
+//
+//  Created by Nicole Sliwa on 2/25/15.
+//  Copyright (c) 2015 Team B.E.N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
